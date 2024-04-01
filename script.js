6f4224d927594dae4da7402fae4f1b6cef9675c4
Allison <124654013+dyolf-walrus@users.noreply.github.com>
2024-03-31 23:03:44 -0400

    create UI and continue script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+let player1;
+let player2;
+
 const Gameboard = (() => {
     let board = [0, 1, 2,
                 3, 4, 5,
                 6, 7, 8];
     
-    function addMarker(index, marker) {
-        board[index] = marker;
+    function addMarker(index, player) {
+        if (board[index] === player1.marker || board[index] === player2.marker) {
+            return 'Please choose an empty space to place your marker on'
+        } else {
+            board[index] = player.marker;
+            return `${player.name} has placed their marker.`
+        }
     }
 
     function showBoard() {
@@ -36,7 +44,13 @@ const Gameboard = (() => {
 })();
 
 function createPlayer(name, marker) {
-    return {name, marker}
+    let wins = 0
+
+    function increaseWins() {
+        wins++;
+    }
+
+    return {name, marker, wins, increaseWins}
 }
 
 const gamePlay = (() => {
@@ -51,17 +65,78 @@ const gamePlay = (() => {
         }
     }
 
-    return {declareWinner};
+    let round = 1;
+
+    function increaseRound() {
+        round++;
+    }
+
+    function resetRound() {
+        round = 1;
+    }
+
+    return {declareWinner, round, increaseRound, resetRound};
 })();
 
-const player1 = createPlayer('Betty1', 'X');
-const player2 = createPlayer('Jessica2', 'O');
+document.getElementById("get-player-info").addEventListener("submit", function (e) {
+    e.preventDefault();
+    let form = document.getElementById("get-player-info");
+  
+    let formData = new FormData(form);
+    let playerInfo = Object.fromEntries(formData);
 
-Gameboard.showBoard();
-Gameboard.addMarker(1, player1.marker);
-Gameboard.addMarker(2, player2.marker);
-gamePlay.declareWinner();
-Gameboard.addMarker(4, player1.marker);
-Gameboard.addMarker(7, player1.marker);
-Gameboard.showBoard();
-gamePlay.declareWinner();
+    player1 = createPlayer(playerInfo.p1name, playerInfo.p1mark);
+    player2 = createPlayer(playerInfo.p2name, playerInfo.p2mark);
+
+    updatePlayerDisplays();
+    updateMiddleInfo();
+    allowAddMarkers();
+  });
+
+function updatePlayerDisplays() {
+    let p1Info = document.getElementById("p1-info");
+    let p2Info = document.getElementById("p2-info");
+
+    p1Info.innerHTML = `<div id="player1Stats"><p>${player1.name}</p>
+        <p>Marker: ${player1.marker}</p>
+        <p>Wins: ${player1.wins}</p></div>`;
+
+    p2Info.innerHTML = `<div id="player2Stats"><p>${player2.name}</p>
+        <p>Marker: ${player2.marker}</p>
+        <p>Wins: ${player2.wins}</p></div>`;
+}
+
+function updateMiddleInfo() {
+    let middleInfo = document.getElementById("middle-info");
+
+    middleInfo.innerHTML = `<h1>Round ${gamePlay.round}</h1>`;
+    if (gamePlay.round%2 == 0) {
+        middleInfo.innerHTML += `${player2.name}'s turn!`;
+    } else {
+        middleInfo.innerHTML += `${player1.name}'s turn!`;
+    }
+}
+
+function allowAddMarkers() {
+    let gamesquares = document.getElementsByClassName('gamesquare');
+    //console.log(gamesquares)
+
+    for (space of gamesquares) {
+        space.addEventListener("click", function(e) {
+            let number = parseInt(e.target.dataset.square);
+            console.log(number);
+            console.log(gamePlay.round);
+            if (gamePlay.round%2 === 0) {
+                Gameboard.addMarker(number, player2);
+                gamesquares[number].innerHTML = `${player2.marker}`;
+            } else {
+                Gameboard.addMarker(number, player1);
+                gamesquares[number].innerHTML = `${player1.marker}`;
+            }
+            gamesquares[number].classList.remove("empty");
+            Gameboard.showBoard();
+            gamePlay.round++;
+            console.log(Gameboard.checkForWinner());
+        })
+    }
+}
